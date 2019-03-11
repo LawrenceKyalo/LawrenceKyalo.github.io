71e482bb9d57066422caf8d3822293bb3774f9c6
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2019-03-11 15:44:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 onload(){
 	console.log("Ready");
 }
+ function getCurrentImage(){
+ 	if(date >= current_date){
+		current_image = image.getByDate(lastDate);
+	} else{
+		current_image = image.getByDate(current_date);
+	}
+	 
+	this.current_image = current_image;
+	 return current_image;
+ }
+
+updateImage(){
+	var current_image;
+	var new_image;
+	
+	temp = this.get(current_image);
+	new = this.get(new_image);
+	
+	temp.replace(new);
+		     
+	return new_image;
+}
