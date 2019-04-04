f67761159c448f8ea3e669bcdad524f59ac8c786
Lawrence <lawrencekkyalo@gmail.com>
2019-04-04 14:51:38 +0300

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -2,6 +2,7 @@ var app = angular.module('Portfolio', []);
 app.controller('PortfolioCtrl', function($scope){
 	$scope.name = "Lawrence Kyalo";
 	$scope.title = "Lead Developer";
+	$scope.value = {{name + ", " + title}};
 
 app.directive(function($scope){
 	return template: "<div> {{ $value }} </div>"
